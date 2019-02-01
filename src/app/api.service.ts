3f4c2c0be521350d6e1546e13e4c76ef3d72d67e
Catalina Peralta <cperalta@nearshoremx.com>
2019-02-01 10:16:57 -0600

    Updated function descriptions

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,27 +8,33 @@ import { Observable } from 'rxjs';
 
 export class ApiService {
 
+	//Base urls for development and production environments
 	baseurl_dev = "http://localhost:8000";
 	baseurl = "https://peaceful-lake-83171.herokuapp.com";
 	url = "";
 	
+	//HTTP headers sent with each request
 	httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
   	constructor(private http: HttpClient) { }
 
+  	//Service to get all insurer information registered in the app
 	getAllInsurers(): Observable<any>{
 		return this.http.get(this.baseurl + '/api/insurers/', {headers: this.httpHeaders});
 	}
 
+	//Service to get all information related to risks
 	getAllRisks(): Observable<any>{
 		return this.http.get(this.baseurl + '/api/risks/', {headers: this.httpHeaders});
 	}
 
-	getSingleRiskField(id): Observable<any>{
-		this.url = this.baseurl + '/api/riskfields/?search='+id;
+	//Service to return information related to one particular risk field
+	getSingleRiskField(param): Observable<any>{
+		this.url = this.baseurl + '/api/riskfields/?search='+param;
 		console.log(this.url);
 		return this.http.get(this.url, {headers: this.httpHeaders});
 	}
 
+	//Service to return all additional risk fields that have been registered in the app
 	getAllRiskFields(): Observable<any>{
 		return this.http.get(this.baseurl + '/api/riskfields/', {headers: this.httpHeaders});
 	}
